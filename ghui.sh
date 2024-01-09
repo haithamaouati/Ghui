3a38721c25669862df702026467803edcb9fc9c3
Haitham Aouati <55059470+haithamaouati@users.noreply.github.com>
2024-01-09 01:16:46 +0100

    Update ghui.sh

--- ghui.sh
+++ ghui.sh
@@ -14,15 +14,18 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
-USERNAME=$1                                             ACCESS_TOKEN="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"
+USERNAME=$1
+ACCESS_TOKEN="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"
 API_URL="https://api.github.com/users/$USERNAME"        
 response=$(curl -s -H "Authorization: token $ACCESS_TOKEN" $API_URL)
-                                                        if [ -z "$response" ]; then
+if [ -z "$response" ]; then
   echo "Failed to fetch user information. Please check the username and try again."
   exit 1
 fi
 
-echo "Username: $(echo "$response" | jq -r .login)"     echo "Name: $(echo "$response" | jq -r .name)"
+echo "Username: $(echo "$response" | jq -r .login)"
+echo -e "\n"
+echo "Name: $(echo "$response" | jq -r .name)"
 echo "Email: $(echo "$response" | jq -r .email)"
 echo "Bio: $(echo "$response" | jq -r .bio)"
 echo "Location: $(echo "$response" | jq -r .location)"
